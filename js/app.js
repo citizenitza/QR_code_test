a89ff7795d6b25f81667369c5f009bd26232bfaf
Dániel Marcell <daniel.marcell171@gmail.com>
2022-03-06 16:40:29 +0100

    fdf

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function initClient() {
     //Listen for sign-in state changes.                    
     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);         //Handle the initial sign-in state.        
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());        
-    authorizeButton.onclick = handleAuthClick;
+    // authorizeButton.onclick = handleAuthClick;
     // signoutButton.onclick = handleSignoutClick;  
   });
 }
