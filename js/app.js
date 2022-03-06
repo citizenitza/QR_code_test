f53540773ddf985ba019f42a47ecef358b076dac
Dániel Marcell <daniel.marcell171@gmail.com>
2022-03-06 18:37:58 +0100

    new

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-new RGraph.Sheets('14Z_s-FR-YUCb5gX43UVwEtaYuCfGCQj4xo3HdwgkVZI', 'AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', 'Bar chart', function (sheet)
+new RGraph.Sheets('AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', '14Z_s-FR-YUCb5gX43UVwEtaYuCfGCQj4xo3HdwgkVZI', 'Bar chart', function (sheet)
 {
     var data   = sheet.get('B2:B13');
     var labels = sheet.get('A2:A13');
