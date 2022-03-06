d85401e5e5f101e189a69244f099660286133100
Dániel Marcell <daniel.marcell171@gmail.com>
2022-03-06 13:26:59 +0100

    apikey

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Client ID and API key from the Developer Console
-var CLIENT_ID = '<YOUR_CLIENT_ID>';
-var API_KEY = '<YOUR_API_KEY>';// Array of API discovery doc URLs for APIs used by the quickstart
+var CLIENT_ID = '673977278968-rsiltbqfvebrh542ociki8j80oaqkbto.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyB-ybvth_3uZufoWf1Wb_I23W4VKZi7WgA';// Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS =["https://sheets.googleapis.com/$discovery/rest?version=v4"];//Authorization scopes required by the API; 
 
 var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly"; 
