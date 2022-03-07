eede19efc150fb91fc3537ecc793022deb960109
Dániel Marcell <daniel.marcell171@gmail.com>
2022-03-07 20:27:21 +0100

    landing page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-new RGraph.Sheets('AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', '14Z_s-FR-YUCb5gX43UVwEtaYuCfGCQj4xo3HdwgkVZI', 'Bar chart', function (sheet)
+
+new RGraph.Sheets('AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', '14Z_s-FR-YUCb5gX43UVwEtaYuCfGCQj4xo3HdwgkVZI', 'Page', function (sheet)
 {
     var data   = sheet.get('B2:B13');
     var labels = sheet.get('A2:A13');
@@ -8,3 +9,29 @@ new RGraph.Sheets('AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', '14Z_s-FR-YUCb5gX43
     }
 
 });
+
+
+//URL: Single_request.html?index=1
+function GetData(){
+    new RGraph.Sheets('AIzaSyAbK3O6hns-yEQ12zyGbJhR9AtWt713Wis', '14Z_s-FR-YUCb5gX43UVwEtaYuCfGCQj4xo3HdwgkVZI', 'Page', function (sheet){
+        var index = window.location.search.substring(1).split("=")[1];
+        var sheetindex = 0;
+        while(sheet.get('A' + sheetindex) != index){
+            sheetindex++;
+            if(sheetindex > 20000){
+                return false;
+            }
+        }
+        
+        var labels = sheet.get('A2:A13');
+
+        ;
+        var Code = sheet.get('D'+ sheetindex);
+        var Stock= sheet.get('J'+ sheetindex);;
+        var Price= sheet.get('T'+ sheetindex);;
+        document.getElementById("Code").innerHTML = Code;
+        document.getElementById("Stock").innerHTML = Stock;
+        document.getElementById("Price").innerHTML = Price;
+
+    });
+}
